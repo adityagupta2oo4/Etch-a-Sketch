25d2fb613566bf519b245983ffefeac63e276285
Aditya Gupta <ag75100@gmail.com>
2025-02-23 09:12:16 +0000

    Improved the CSS

--- script.js
+++ script.js
@@ -7,38 +7,35 @@ const body = document.querySelector("body");
 container.classList.add("container")
 body.appendChild(container);
 
-//getting the input from the user how many grid he want
-
-
-
-
+// hieght and width is fixed user choose how many grid they want
 
 function createGrid(row,col){
 
-    //setting the grid
+    //setting the indivual grid size
     let box_base = (700)/col;
     let box_hieght = (500)/row ;
     
     
-    
+    // total grid
     let total = row*col;
 
     for(let i = 0 ; i<total; i++){
 
-
+        // to append
         const div = document.createElement("div");
         
         
-
+        //dynamically setting the size of the grid based on user input
         div.style.setProperty("width",`${box_base}px`);
         div.style.setProperty("height" ,`${box_hieght}px`);
-        div.classList.add(`${i}`);
+        div.classList.add(`${i}`); // adding class
         container.appendChild(div);
 
 
             
-
+        // adding the event listener for color
         div.addEventListener("mouseover",() => {
+            // from the userr wwhich has return type of string #____
             let color = document.getElementById("colorPicker").value;
             div.style.backgroundColor = `${color}`;
         });
